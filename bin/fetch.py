21928ec22cff5b29322bfa6fa3629043f0e4e8da
David Cramer <dcramer@gmail.com>
2010-12-06 17:57:35 -0800

    Store forums with posts

--- bin/fetch.py
+++ bin/fetch.py
@@ -16,10 +16,10 @@ conn = sqlite3.connect('db.sqlite')
 conn.isolation_level = None
 
 FEEDS = (
-    # ('DISQUS_USERNAME', 'FEED URL'),
-    ('zeeg', 'http://www.davidcramer.net/disqus/feed'),
-    ('dz', 'http://blog.nodnod.net/tagged/disqus/rss'),
-    ('disqus', 'http://blog.disqus.com/tagged/dev/rss'),
+    # ('DISQUS USERNAME', 'DISQUS FORUM SHORTNAME', 'FEED URL'),
+    ('zeeg', 'davidcramer', 'http://www.davidcramer.net/disqus/feed'),
+    ('dz', 'nodnod', 'http://blog.nodnod.net/tagged/disqus/rss'),
+    ('disqus', 'disqus', 'http://blog.disqus.com/tagged/dev/rss'),
 )
 
 def slugify(value):
@@ -44,17 +44,17 @@ def linebreaks(value, autoescape=False):
     return u'\n\n'.join(paras)
 
 class FeedAggregator(object):
-    def collect(self, author, feed_url): 
+    def collect(self, author, forum, feed_url):
         feed = from_url(feed_url)
         for entry in feed.entries:
             cursor = conn.cursor()
             cursor.execute('select 1 from entries where url = ? limit 1', [unicode(entry.link)])
             if not cursor.fetchall():
                 slug = '%s-%s' % (author, slugify(unicode(entry.title)))
-                self.write(author, entry.link, entry.title, entry.description, entry.published, slug)
+                self.write(author, forum, entry.link, entry.title, entry.description, entry.published, slug)
                 cursor.execute('insert into entries values(?)', [unicode(entry.link)])
     
-    def write(self, disqus_username, url, title, body, date=None, slug=None):
+    def write(self, disqus_username, disqus_forum, url, title, body, date=None, slug=None):
         print "Saving", url
         if not date:
             date = datetime.datetime.now()
@@ -72,6 +72,7 @@ class FeedAggregator(object):
                    .replace('}', '&#125;')
         
         data = {
+            'disqus_forum': disqus_forum,
             'title': title,
             'url': url,
             'slug': slug,
@@ -87,9 +88,9 @@ def main():
     conn.execute('create table if not exists entries (url text)')
     
     agg = FeedAggregator()
-    for author, feed in FEEDS:
+    for author, forum, feed in FEEDS:
         try:
-            agg.collect(author, feed)
+            agg.collect(author, forum, feed)
         except Exception, e:
             logging.exception(e)
 
